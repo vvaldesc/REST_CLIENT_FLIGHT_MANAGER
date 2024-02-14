42ce242e8053e3a41bb5ffb36a752757fa0eeee9
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2024-02-15 00:38:04 +0100

    Menu superior

--- templates/generic_Templates.php
+++ templates/generic_Templates.php
@@ -16,6 +16,10 @@ function entornoH3($param) {
     return '<h3>' . $param . '</h3>';
 }
 
+function entornoTbody($param) {
+    return '<tbody>' . $param . '</tbody>';
+}
+
 function entornoThead($param) {
     return '<thead>' . $param . '</thead>';
 }
@@ -36,3 +40,23 @@ function mostrar($param) {
     echo $param;
 }
 
+function menuSuperior() {
+    return '<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item m-2">
+          <a class="nav-link btn btn-primary btn-block" href="'.$_SERVER["DOCUMENT_ROOT"].'">Lista de vuelos</a>
+        </li>
+        <li class="nav-item m-2">
+          <a class="nav-link btn btn-primary btn-block" href="'.$_SERVER["DOCUMENT_ROOT"].'?controller=pasajes&action=interfazPasajes'.'">Insertar pasaje</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>';
+}
